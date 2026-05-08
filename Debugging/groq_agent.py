737d2ad169531c6993455f666416424dc982dc72
Yuval Varke <varke.vishu01@gmail.com>
2026-05-08 11:09:02 +0530

    Added tool calling agent

--- Debugging/groq_agent.py
+++ Debugging/groq_agent.py
@@ -36,4 +36,35 @@ def make_default_graph():
     agent=graph_workflow.compile()
     return agent
 
-agent=make_default_graph()
+
+def make_alternative_graph():
+    """Make a tool-calling agent"""
+
+    @tool
+    def add(a: float, b: float):
+        """Adds two numbers."""
+        return a + b
+
+    tool_node = ToolNode([add])
+    model_with_tools = llm_groq.bind_tools([add])
+    def call_model(state):
+        return {"messages": [model_with_tools.invoke(state["messages"])]}
+
+    def should_continue(state: State):
+        if state["messages"][-1].tool_calls:
+            return "tools"
+        else:
+            return END
+
+    graph_workflow = StateGraph(State)
+
+    graph_workflow.add_node("agent", call_model)
+    graph_workflow.add_node("tools", tool_node)
+    graph_workflow.add_edge("tools", "agent")
+    graph_workflow.add_edge(START, "agent")
+    graph_workflow.add_conditional_edges("agent", should_continue)
+
+    agent = graph_workflow.compile()
+    return agent
+
+agent=make_alternative_graph()
